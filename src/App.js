c535aa498cfefa3d757ebda91881a041bc7b2971
Josh Justice <me@codingitwrong.com>
2020-10-04 06:32:46 -0400

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import initializeAxe from './axe';
 
 initializeAxe();
 
 const App = () => {
   return (
-    <>
+    <Router>
       <header>
         <h1>Reactup App</h1>
       </header>
       <main>
         <p>Hello, world.</p>
       </main>
-    </>
+    </Router>
   );
 };
 

b262d9c6e1a1012bf2a4a655c1b2fd7b2315725b
Josh Justice <me@codingitwrong.com>
2020-10-05 06:57:18 -0400

    SQUASH fix store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,8 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import {devToolsEnhancer} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import example1 from './examples/example1/store/reducer';
 
-const rootReducer = combineReducers({
-  example1,
-});
+const rootReducer = combineReducers({});
 
 const store = createStore(
   rootReducer,

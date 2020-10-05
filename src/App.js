5b893a037182e058e1eb6827c105d7b192fbb458
Josh Justice <me@codingitwrong.com>
2020-10-05 08:17:37 -0400

    Describe examples

--- src/App.js
+++ src/App.js
@@ -37,21 +37,25 @@ const App = () => {
                   <Link to="/unprepared/example1" data-cy="example1">
                     Example 1
                   </Link>
+                  - Trivial Failure
                 </li>
                 <li>
                   <Link to="/unprepared/example2" data-cy="example2">
                     Example 2
                   </Link>
+                  - Trivial Solution
                 </li>
                 <li>
                   <Link to="/unprepared/example3" data-cy="example3">
                     Example 3
                   </Link>
+                  - Redux Failure
                 </li>
                 <li>
                   <Link to="/unprepared/example4" data-cy="example4">
                     Example 4
                   </Link>
+                  - Redux Solution
                 </li>
               </ul>
             </li>
@@ -62,21 +66,25 @@ const App = () => {
                   <Link to="/flickering/example5" data-cy="example5">
                     Example 5
                   </Link>
+                  - Trivial Failure
                 </li>
                 <li>
                   <Link to="/flickering/example6" data-cy="example6">
                     Example 6
                   </Link>
+                  - Trivial Solution
                 </li>
                 <li>
                   <Link to="/flickering/example7" data-cy="example7">
                     Example 7
                   </Link>
+                  - Redux Failure
                 </li>
                 <li>
                   <Link to="/flickering/example8" data-cy="example8">
                     Example 8
                   </Link>
+                  - Redux Solution
                 </li>
               </ul>
             </li>
@@ -87,31 +95,37 @@ const App = () => {
                   <Link to="/impatient/example9" data-cy="example9">
                     Example 9
                   </Link>
+                  - Trivial Failure
                 </li>
                 <li>
                   <Link to="/impatient/example10" data-cy="example10">
                     Example 10
                   </Link>
+                  - Instant-State-Change Solution
                 </li>
                 <li>
                   <Link to="/impatient/example11" data-cy="example11">
                     Example 11
                   </Link>
+                  - Loading-State Solution
                 </li>
                 <li>
                   <Link to="/impatient/example12" data-cy="example12">
                     Example 12
                   </Link>
+                  - Green-Light Solution
                 </li>
                 <li>
                   <Link to="/impatient/example13" data-cy="example13">
                     Example 13
                   </Link>
+                  - Redux Failure
                 </li>
                 <li>
                   <Link to="/impatient/example14" data-cy="example14">
                     Example 14
                   </Link>
+                  - Redux Loading-State Solution
                 </li>
               </ul>
             </li>

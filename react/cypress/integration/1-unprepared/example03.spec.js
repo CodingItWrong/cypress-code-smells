4c03e8fa680eaf98e4eacae45d529454545b3a6e
Josh Justice <me@codingitwrong.com>
2020-10-06 08:06:43 -0400

    Move react app into subfolder

--- react/cypress/integration/1-unprepared/example03.spec.js
+++ react/cypress/integration/1-unprepared/example03.spec.js
@@ -1,5 +1,12 @@
 describe('Example 3', () => {
   it('fails with Redux', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: '/api/count.json',
+      response: {count: 27},
+    });
+
     cy.visit('/unprepared/example3');
     cy.get('[data-cy=increment]').click();
     cy.get('[data-cy=count]').contains(28);

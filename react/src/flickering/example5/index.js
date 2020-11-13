a8d791ae67883b6e72a943dc91e55fe6982ebfa7
Josh Justice <josh.justice@accesscfa.com>
2020-11-13 13:31:56 -0500

    Remove comments for demo

--- react/src/flickering/example5/index.js
+++ react/src/flickering/example5/index.js
@@ -4,7 +4,6 @@ export default function Example5() {
   const [loading, setLoading] = useState(false);
   const [count, setCount] = useState(null);
 
-  // NOTE: useLayoutEffect fixes it
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
